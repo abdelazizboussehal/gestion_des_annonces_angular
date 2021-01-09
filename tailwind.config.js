b658f0c8efbcb636ec05197eeb3583b5892e5fb3
abdelaziz.boussehal <abdelaziz.boussehal@univ-constantine2.dz>
2021-01-09 20:03:59 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.ts', './projects/**/*.html', './projects/**/*.ts'],
+  theme: {
+    extend: {
+      colors: {
+        aziz: {
+          100: '#3c3f41',
+          200: '#415357'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
